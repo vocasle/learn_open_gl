81c0cbd2871b54a1822e9936de0b4546026da1d9
Nikita Elsakov <vocasle@gmail.com>
2021-11-07 20:53:26 +0300

    Change amount of visibility of textures by pressing up or down key

--- src/1.getting_started/4.1.textures/4.1.shader.fs
+++ src/1.getting_started/4.1.textures/4.1.shader.fs
@@ -5,11 +5,12 @@ in vec3 color;
 
 uniform sampler2D out_texture;
 uniform sampler2D out_texture2;
+uniform float in_ratio;
 
 out vec4 out_color;
 
 void main()
 {
     vec2 reversed_face_coord = vec2(-texture_coord.x, texture_coord.y);
-    out_color = mix(texture(out_texture, texture_coord), texture(out_texture2, reversed_face_coord), 0.5);
+    out_color = mix(texture(out_texture, texture_coord), texture(out_texture2, reversed_face_coord), in_ratio);
 }
--- src/1.getting_started/4.1.textures/textures.cpp
+++ src/1.getting_started/4.1.textures/textures.cpp
@@ -5,28 +5,42 @@
 #include <GLFW/glfw3.h>
 
 #include "shader.h"
-#include "../../../thirdparties/stb_image/include/stb_image.h"
+#include "stb_image.h"
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+struct InputData {
+    bool toggle_fill_mode;
+    float ratio;
+};
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window) {
-    static bool fill = true;
+void processInput(GLFWwindow* window, InputData& input_data)
+{
     static bool e_pressed = false;
 
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE)==GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+    else if (glfwGetKey(window, GLFW_KEY_E)==GLFW_PRESS)
         e_pressed = true;
-    else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_RELEASE && e_pressed) {
-        fill = !fill;
-        glPolygonMode(GL_FRONT_AND_BACK, fill ? GL_FILL : GL_LINE);
+    else if (glfwGetKey(window, GLFW_KEY_E)==GLFW_RELEASE && e_pressed) {
+        input_data.toggle_fill_mode = !input_data.toggle_fill_mode;
         e_pressed = false;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_UP)==GLFW_PRESS) {
+        input_data.ratio += 0.01f;
+        input_data.ratio = input_data.ratio > 1.0f ? 1.0f : input_data.ratio;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_DOWN)==GLFW_PRESS) {
+        input_data.ratio -= 0.01f;
+        input_data.ratio = input_data.ratio <= 0.0f ? 0.0f : input_data.ratio;
     }
 }
 
-int main() {
+int main()
+{
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
@@ -35,7 +49,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -52,10 +66,10 @@ int main() {
 
     constexpr float vertices[] = {
             // positions          // colors           // texture coords
-            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.55f, 0.55f,   // top right
-            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.55f, 0.55f,   // bottom right
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.45f, 0.45f,   // bottom left
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.45f, 0.55f    // top left
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // bottom left
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f    // top left
     };
 
     constexpr uint indices[] = {
@@ -84,7 +98,7 @@ int main() {
     int32_t width;
     int32_t height;
     int32_t nr_channels;
-    unsigned char *data = stbi_load("../assets/container.jpg", &width, &height, &nr_channels, 0);
+    unsigned char* data = stbi_load("../assets/container.jpg", &width, &height, &nr_channels, 0);
     if (!data)
         std::cerr << "Failed to load texture from ../assets/container.jpg" << std::endl;
 
@@ -115,11 +129,11 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void*>(nullptr));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), static_cast<void*>(nullptr));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), reinterpret_cast<void*>(3*sizeof(float)));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), reinterpret_cast<void*>(6*sizeof(float)));
     glEnableVertexAttribArray(2);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -128,13 +142,18 @@ int main() {
     shader.set_int("out_texture", 0);
     shader.set_int("out_texture2", 1);
 
+    InputData input_data = {false, 0.0f};
+
     while (!glfwWindowShouldClose(window)) {
-        processInput(window);
+        processInput(window, input_data);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
+
+        glPolygonMode(GL_FRONT_AND_BACK, input_data.toggle_fill_mode ? GL_FILL : GL_LINE);
+        shader.set_float("in_ratio", input_data.ratio);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
