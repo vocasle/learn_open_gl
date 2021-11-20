07cfa0cf2ee2c188cc7578160ec882065313b008
Nikita Elsakov <vocasle@gmail.com>
2021-11-20 20:54:40 +0300

    Remove unused code

--- src/1.getting_started/5.1.transformations/transformations.cpp
+++ src/1.getting_started/5.1.transformations/transformations.cpp
@@ -38,41 +38,45 @@ enum class GridSize : uint8_t {
   SMALL = 8, MEDIUM = 16, BIG = 32
 };
 
-struct Controller {
-  static constexpr float grid_zoom_levels[] = {-8.0f, -40.0f, -15.0f};
-  Direction direction;
-  Direction prev_direction;
-  glm::mat4 model;
+struct Camera {
   glm::mat4 view;
   glm::mat4 projection;
-  Difficulty difficulty;
-  GridSize grid_size;
-  std::string song;
-  double tick_time;
 };
 
-void update_difficulty(Controller &c, Difficulty d) {
+struct GameState {
+  double start_time;
+  double end_time;
+  double tick_time;
+  bool game_over;
+  Difficulty difficulty;
+  GridSize grid_size;
+  static constexpr float grid_zoom_levels[] = {-8.0f, -15.0f, -40.0f};
+  std::string song;
+
+  Direction direction;
+  Direction prev_direction;
+
+  Camera camera;
+};
+
+float get_zoom_level(const GameState &g);
+
+void update_difficulty(GameState &g, Difficulty d) {
   std::cout << "Updating difficulty to " << static_cast<int>(d) << std::endl;
   switch (d) {
-  case Difficulty::LOW:c.song = "../assets/Greenberg.mp3";
-    c.grid_size = GridSize::SMALL;
-    c.tick_time = 1.0;
+  case Difficulty::LOW:g.song = "../assets/Greenberg.mp3";
+    g.grid_size = GridSize::SMALL;
+    g.tick_time = 1.0;
     break;
-  case Difficulty::MEDIUM:c.song = "../assets/Drivin.mp3";
-    c.grid_size = GridSize::MEDIUM;
-    c.tick_time = 0.5;
+  case Difficulty::MEDIUM:g.song = "../assets/Drivin.mp3";
+    g.grid_size = GridSize::MEDIUM;
+    g.tick_time = 0.5;
     break;
-  case Difficulty::HIGH:c.song = "../assets/Eastward.mp3";
-    c.grid_size = GridSize::BIG;
-    c.tick_time = 0.25;
+  case Difficulty::HIGH:g.song = "../assets/Eastward.mp3";
+    g.grid_size = GridSize::BIG;
+    g.tick_time = 0.25;
     break;
   }
-}
-
-void init_controller(Controller &c) {
-  c.direction = Direction::NONE;
-  c.prev_direction = Direction::NONE;
-  update_difficulty(c, Difficulty::LOW);
 }
 
 struct Point {
@@ -82,7 +86,7 @@ struct Point {
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
-void process_input(GLFWwindow *window, Controller &controller);
+void process_input(GLFWwindow *window, GameState &state);
 
 GLFWwindow *init_glfw();
 
@@ -90,18 +94,16 @@ VertexData init_vertices();
 
 void play_audio(const char *file_name);
 
-void setup_uniforms(const Shader &shader, Controller &controller);
-
-Point update_pos(const Point &pos, Controller &c) {
+Point update_pos(const Point &pos, GameState &state) {
   Point cur_pos = pos;
 
-  if (c.direction == Direction::RIGHT && c.prev_direction != Direction::LEFT)
+  if (state.direction == Direction::RIGHT && state.prev_direction != Direction::LEFT)
     ++cur_pos.x;
-  else if (c.direction == Direction::LEFT && c.prev_direction != Direction::RIGHT)
+  else if (state.direction == Direction::LEFT && state.prev_direction != Direction::RIGHT)
     --cur_pos.x;
-  else if (c.direction == Direction::UP && c.prev_direction != Direction::DOWN)
+  else if (state.direction == Direction::UP && state.prev_direction != Direction::DOWN)
     ++cur_pos.y;
-  else if (c.direction == Direction::DOWN && c.prev_direction != Direction::UP)
+  else if (state.direction == Direction::DOWN && state.prev_direction != Direction::UP)
     --cur_pos.y;
   return cur_pos;
 }
@@ -204,6 +206,7 @@ VertexData init_grid_vertices() {
 
 void draw_grid(Shader &shader, const VertexData &vertex_data) {
   shader.use();
+  shader.set_mat4("model", glm::mat4(1.0f));
   glBindVertexArray(vertex_data.VAO);
   glDrawArrays(GL_LINES, 0, vertex_data.vertices.size());
 }
@@ -215,8 +218,7 @@ void print_snake_pos(const std::deque<Point> &snake, const Point &meal) {
   std::cout << std::endl;
 }
 
-void draw_snake(const std::deque<Point> &deque_1, const Shader &shader, const VertexData &vertex_data,
-                const Controller &controller) {
+void draw_snake(const std::deque<Point> &deque_1, const Shader &shader, const VertexData &vertex_data) {
   shader.use();
 
   for (auto p: deque_1) {
@@ -231,12 +233,6 @@ void draw_snake(const std::deque<Point> &deque_1, const Shader &shader, const Ve
 
 }
 
-struct GameState {
-  double start_time;
-  double end_time;
-  bool game_over;
-};
-
 void draw_meal(const Shader &shader, const VertexData &vertex_data, const Point &meal) {
   shader.use();
   glBindVertexArray(vertex_data.VAO);
@@ -249,114 +245,98 @@ void init_game_state(GameState &state) {
   state.game_over = false;
   state.end_time = 0.0;
   state.start_time = glfwGetTime();
+
+  state.camera.projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);;
+  state.camera.view = glm::translate(glm::mat4(1.0f),
+                                     glm::vec3(0.0f, 0.0f, get_zoom_level(state)));
+
+  state.direction = state.prev_direction = Direction::NONE;
+  update_difficulty(state, Difficulty::LOW);
+
 }
 
-bool is_frame_passed(const GameState &state, const Controller &c)
-{
-  return state.end_time - state.start_time >  c.tick_time;
+bool is_frame_passed(const GameState &state) {
+  return state.end_time - state.start_time > state.tick_time;
 }
 
-//bool is_
+void setup_uniforms(const Shader &shader, GameState &state);
 
 int main() {
   GLFWwindow *window = init_glfw();
   if (!window)
     return -1;
 
-  Controller c;
-  init_controller(c);
-  Controller c2;
-  init_controller(c2);
   Shader shader("5.1.shader.vs", "5.1.shader.fs");
-  Shader line_shader("5.1.line_shader.vs", "5.1.line_shader.fs");
+  Shader grid_shader("5.1.line_shader.vs", "5.1.line_shader.fs");
   VertexData vertex_data = init_vertices();
-  setup_uniforms(line_shader, c);
-  setup_uniforms(shader, c2);
+  VertexData line_vertex_data = init_grid_vertices();
+
   GameState game;
   init_game_state(game);
-
   std::deque<Point> snake_parts(1, {1, 1});
 
-////    std::thread audio(play_audio, "../assets/Greenberg.mp3");
+//    std::thread audio(play_audio, "../assets/Greenberg.mp3");
 
-  // Make snake peace twice smaller to fit into the grid and move to the
-  VertexData line_vertex_data = init_grid_vertices();
-  c2.model = glm::scale(c2.model, glm::vec3(0.5));
-  c2.model = glm::translate(c2.model, glm::vec3(1.0f, 1.0f, 0.0f));
-  shader.set_mat4("model", c2.model);
-  constexpr int grid_size = 8;
-  Point meal = spawn_meal(grid_size, snake_parts);
+  Point meal = spawn_meal(static_cast<int>(game.grid_size), snake_parts);
   bool ate_meal = false;
-  constexpr double tick_interval = 1.0;
 
   while (!glfwWindowShouldClose(window)) {
-    process_input(window, c2);
-    process_input(window, c);
+    setup_uniforms(shader, game);
+    setup_uniforms(grid_shader, game);
+    process_input(window, game);
     game.end_time = glfwGetTime();
 
-    if (is_frame_passed(game, c) && !game.game_over) {
+    if (is_frame_passed(game) && !game.game_over) {
       std::cout << "Frame took: " << game.end_time - game.start_time << std::endl;
       if (ate_meal)
-        meal = spawn_meal(grid_size, snake_parts);
+        meal = spawn_meal(static_cast<int>(game.grid_size), snake_parts);
 
-      Point p = update_pos(snake_parts[0], c2);
+      Point p = update_pos(snake_parts[0], game);
       ate_meal = collided_with_meal(p, meal);
       move_snake(p, snake_parts, ate_meal);
-//      print_snake_pos(snake_parts, meal);
-      game.game_over = is_of_the_grid(p, grid_size) || collided_with_self(snake_parts);
+      game.game_over = is_of_the_grid(p, static_cast<int>(game.grid_size)) || collided_with_self(snake_parts);
       game.start_time = game.end_time;
     }
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    draw_grid(line_shader, line_vertex_data);
-    draw_snake(snake_parts, shader, vertex_data, c2);
+    draw_grid(grid_shader, line_vertex_data);
+    draw_snake(snake_parts, shader, vertex_data);
     if (!ate_meal)
       draw_meal(shader, vertex_data, meal);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
 
-    c2.prev_direction = c2.direction;
+    game.prev_direction = game.direction;
   }
 
   glfwTerminate();
 }
 
-float get_zoom_level(const Controller &c) {
-  switch (c.grid_size) {
-  case GridSize::MEDIUM:return c.grid_zoom_levels[1];
-  case GridSize::BIG:return c.grid_zoom_levels[2];
-  default:return c.grid_zoom_levels[0];
+float get_zoom_level(const GameState &g) {
+  switch (g.grid_size) {
+  case GridSize::SMALL:return g.grid_zoom_levels[0];
+  case GridSize::MEDIUM:return g.grid_zoom_levels[1];
+  case GridSize::BIG:return g.grid_zoom_levels[2];
+  default:return g.grid_zoom_levels[0];
   }
 }
 
-void setup_uniforms(const Shader &shader, Controller &controller) {
-  glm::mat4 model(1.0f);
-  model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-
-  glm::mat4 view(1.0f);
-  view = glm::translate(view,
-                        glm::vec3(0.0f, 0.0f, get_zoom_level(controller)));
-
-  glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
+void setup_uniforms(const Shader &shader, GameState &state) {
   shader.use();
-  shader.set_mat4("model", model);
-  shader.set_mat4("view", view);
-  shader.set_mat4("projection", projection);
-
-  controller.view = view;
-  controller.model = model;
-  controller.projection = projection;
+  state.camera.view = glm::translate(glm::mat4(1.0f),
+                                     glm::vec3(0.0f, 0.0f, get_zoom_level(state)));
+  shader.set_mat4("view", state.camera.view);
+  shader.set_mat4("projection", state.camera.projection);
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
 }
 
-void process_input(GLFWwindow *window, Controller &controller) {
+void process_input(GLFWwindow *window, GameState &state) {
   static bool fill = true;
   static bool e_pressed = false;
 
@@ -369,19 +349,19 @@ void process_input(GLFWwindow *window, Controller &controller) {
     glPolygonMode(GL_FRONT_AND_BACK, fill ? GL_FILL : GL_LINE);
     e_pressed = false;
   } else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-    controller.direction = Direction::UP;
+    state.direction = Direction::UP;
   else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-    controller.direction = Direction::DOWN;
+    state.direction = Direction::DOWN;
   else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-    controller.direction = Direction::LEFT;
+    state.direction = Direction::LEFT;
   else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-    controller.direction = Direction::RIGHT;
+    state.direction = Direction::RIGHT;
   else if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
-    update_difficulty(controller, Difficulty::LOW);
+    update_difficulty(state, Difficulty::LOW);
   else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
-    update_difficulty(controller, Difficulty::MEDIUM);
+    update_difficulty(state, Difficulty::MEDIUM);
   else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
-    update_difficulty(controller, Difficulty::HIGH);
+    update_difficulty(state, Difficulty::HIGH);
 }
 
 VertexData init_vertices() {
